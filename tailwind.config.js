5638de43540f89dab9e42fd64d6e0e5c478ac859
AndrewVis <97456285+AndrewVis@users.noreply.github.com>
2022-05-15 12:30:59 -0400

    fixing dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,7 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: [],
   theme: {
-    letterSpacing: {
-      tight: '-.15em'
-    },
-    extend: {
-      height: {
-        'half-screen': '50vh',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
