913c6f7f91930614b2540642520a76bd5716b4a2
AndrewVis <97456285+AndrewVis@users.noreply.github.com>
2022-05-15 12:03:50 -0400

    clean file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {},
+    letterSpacing: {
+      tight: '-.15em'
+    },
+    extend: {
+      height: {
+        'half-screen': '50vh',
+      },
+    },
   },
   plugins: [],
 }
