6fa28f275c2d7de59049f67fc2e18a43d40e2cee
AndrewVis <97456285+AndrewVis@users.noreply.github.com>
2022-05-16 21:16:48 -0400

    signin started

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Masthead from '../components/masthead'
+import Signin from '../components/signin'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <Masthead />
+      <Signin />
     
       
       <footer className={styles.footer}>
