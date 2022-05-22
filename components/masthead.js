e2ea9783e3a5631e0b8194e27fc83b79b1c45c3a
AndrewVis <97456285+AndrewVis@users.noreply.github.com>
2022-05-22 13:59:12 -0400

    commit prior to beginning form validation / functionality

--- components/masthead.js
+++ components/masthead.js
@@ -15,7 +15,7 @@ export default class Masthead extends React.Component {
           <source src="/assets/videobg.mp4" type="video/mp4;" />
          </video>
         <div className="flex flex-col items-center justify-center">
-             <Image src="/assets/logo.png" width={115} height={115} alt="logo" />
+             <Image src="/assets/logo.png" width={90} height={90} alt="logo" />
          <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0.10)] text-center flex-1 flex items-center justify-center flex-col">
           <h1 className="mb-6 text-6xl xl:text-5xl">Rate Works</h1>
           <h2 className="mb-2 text-3xl xl:text3xl tracking-tight">
--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by Andrew :)
+          Powered by 
          
         </a>
       </footer>
