7514203af50edb73e8c35716df4ae1bbe7b1dd52
AndrewVis <97456285+AndrewVis@users.noreply.github.com>
2022-05-25 10:03:00 -0400

    completed broker page

--- components/aboutus.js
+++ components/aboutus.js
@@ -7,7 +7,7 @@ export default class Aboutus extends React.Component {
       <div className="bg-black text-white">
         <div className="min-h-screen max-w-5xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-4xl md:text-6xl lg:text-7xl tracking-tight font-semibold">
           <div className="leading-[1.15]">
-            <div>About Us</div>
+            <div className="py-10">About Us</div>
             <span>We are breaking the communication barrier in the financial industry.
             </span>
             <span> Modernizing the way <strong>YOU</strong> do business. Creating transparency from end to end to ultimately provide the best client experience.
--- pages/broker.js
+++ pages/broker.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import { useState } from 'react';
 import { createClient } from "@supabase/supabase-js";
 import { async } from "postcss-js";
@@ -28,7 +28,8 @@ return {
 export default function Broker ({data}){
    
     return (
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="bg-neutral-900 max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        {/* <h1 className="mb-2 text-3xl xl:text-3xl p-8 font-bold z-10 text-neutral-300 drop-shadow-[0_5px_3px_rgba(0,0,0.10)] text-center flex-1 flex items-center justify-center flex-col">5 Year Mortgage Rates</h1> */}
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {data.length >0 && data.map(item =><BlurImage item = {item}/>)}
         </div>
@@ -41,20 +42,21 @@ function BlurImage (props) {
   const [isLoading, setLoading] = useState(true)
   console.log(props)
   return (
-    <a href={props.id} className="group">
+    <a href={props.item.id} className="group">
       <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
         <img alt=""
         src={props.item.ImageSrc}
         layout="fill"
         objectFit="cover"
+        onclick=""
         className={(
         "group-hover:opacity-75 duration-700 ease-in-out"
     
         )}
       />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">{props.item.Company}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-700">{props.item.Rate}</p>
+      <h3 className="mt-4 text-lg text-neutral-300 border-2 text-center border-white rounded-2xl">{props.item.Company}</h3>
+      <p className="mt-1 text-lg font-medium text-neutral-300 text-center border-2 border-white rounded-3xl">{props.item.Rate}</p>
     </a>
   )
 }
