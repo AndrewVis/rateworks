ffc55eba66b0485f57d6cc0d06c9ba19e6fdb9c1
AndrewVis <97456285+AndrewVis@users.noreply.github.com>
2022-05-25 16:51:07 -0400

    completed sign in page

--- pages/signin.js
+++ pages/signin.js
@@ -1,21 +1,41 @@
 import React from 'react'
-import styles from '../styles/styles.module.css'
+
 
 class SignIn extends React.Component {
   render () {
   return (
-  <div className={styles.modal_bg}>
-    <div className={styles.modal}>
-      <h2>Sign in!</h2>
-      <label for="name">Username/Email</label>
-      <input type="text"></input>
-      <label for="password">Password</label>
-      <input type="text"></input>
-      <button className="text-gray-700">Sign in</button>
+    <div className="bg-white text-black flex flex-col justify-center pt-10 min-h-screen">
+    <div className="flex-1 flex flex-col justify-center items-center pt=10 lg:pt-6">
+    <div className="pb-10">
+    </div>
+    <h2 className="text-4xl font-bold">Sign in!</h2>
+    <form className="flex flex-col gap-4 mt-16 px-10 lg:mt-20 min-w- lg:min-w-[500]">
+      <input id="companyName" 
+      name="companyName" 
+      required maxLength={50} 
+      type="text" 
+      placeholder="E-mail"
+      className="bg-white text-center text-black outline-none border-2 border-black rounded-3xl px-8 py-2"/>
+      <input id="email" 
+      name="email"
+      type="email"
+      required maxLength={50}
+      placeholder="Password"
+      className="bg-white text-black text-center outline-none border-2 border-black rounded-3xl px-8 py-2"/>
+      <div className="text-center ">
+        <button type="submit" className="bg-black text-white rounded-3xl px-8 py-2">GO</button>
+      </div>
+      <div>
+        <h3 className="text-2xl text-center pt-5 font-bold">OR</h3>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-8 mt-6">
+        {/* <img src="/assets/Twitter_Logo.png" width={90} height={90} alt="logo" /> */}
+        <img src="/assets/LinkedIn_logo_initials.png" width={70} height={30} alt="logo"/>
+        <img src="/assets/Google__G__Logo.svg.webp" width={70} height={30} alt="logo"/>
+      </div>
+    </form>
     </div>
   </div>
-
-
     );
   }
 }
